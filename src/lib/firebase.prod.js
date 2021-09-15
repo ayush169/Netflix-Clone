55fa1f78d2f70d2e83df531e5e8030369115da29
ayush dubey <ayushdubeydps@gmail.com>
2021-09-15 19:37:10 +0530

    React context for firebase

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,20 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/database";
+import { seedDatabase } from "../seed";
+
+// we need to somehow seed the database
+
+// we need a config here
+const config = {
+  apiKey: "AIzaSyCmhivC-tCKiwt_m5Gf2Uuga5bA6M2F58M",
+  authDomain: "netflix-clone-d9cdc.firebaseapp.com",
+  projectId: "netflix-clone-d9cdc",
+  storageBucket: "netflix-clone-d9cdc.appspot.com",
+  messagingSenderId: "245986204380",
+  appId: "1:245986204380:web:3ea6097aeb018e2f123219",
+};
+
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };
