b1eba701ab6f2ea2953905ca898ae8be9fa4f4ef
ayush dubey <ayushdubeydps@gmail.com>
2021-10-12 19:52:58 +0530

    Signin page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Feature, OptForm } from "../components";
-import { HeaderContainer } from "../containers/header";
 import { FaqsContainer } from "../containers/faqs";
 import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
 import { JumbotronContainer } from "../containers/jumbotron";
 
 export default function Home() {
